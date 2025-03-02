116f65b0e6294b064bf83eb5c6aa9d23346a359b
ralphjenrey <loquellanoralphjenrey@gmail.com>
2025-03-03 06:34:21 +0800

    updated Task model

--- src/models/task.ts
+++ src/models/task.ts
@@ -1,8 +1,19 @@
-import { Complaint, Status } from "./complaint";
+import { Complaint } from "./complaint";
 
 export interface Task {
     task_id: string;
-    staff_id: string;
-    status: Status;
+    shift_id: string;
+    status: TaskStatus;
     complaints?: Complaint;  
+    deadline: string;
+}
+
+export enum TaskStatus {
+    PENDING = "PENDING",
+    IN_PROGRESS = "IN PROGRESS",
+    COMPLETED = "COMPLETED",
+}
+
+export function validateStatus(status: string): boolean {
+    return Object.values(TaskStatus).includes(status as TaskStatus);
 }
